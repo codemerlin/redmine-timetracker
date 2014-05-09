e25ba2752d298b85da9a96575a322399582f77ab
Mohit Thakral <mohit@thakral.in>
2014-05-08 22:36:53 -0500

    Message Box and valdiations

--- First.py
+++ First.py
@@ -10,7 +10,7 @@ import ScrolledText
 import RedMineClient as rm
 import threading as threading
 # from ttk import
-
+import mbox
 
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
@@ -30,11 +30,9 @@ class Application(ttk.Frame):
         self.activities = None
         self.time_in_minutes = Tkinter.StringVar()
         self.issue_subject = Tkinter.StringVar()
-        self.issue_subject.set("<<Test Subject>>")
+        self.issue_subject.set("")
         self.issue_id = Tkinter.StringVar()
         self.issue_id.set("")
-        self.status_msg = Tkinter.StringVar()
-        self.status_msg.set("<<Test status Message>>")
         self.issue = None
         self.select_activity = Tkinter.StringVar()
         master.title("Redmine Time Tracker")
@@ -46,6 +44,7 @@ class Application(ttk.Frame):
         self.entry_issue = ttk.Entry(self.main_frame, textvariable=self.issue_id)
         self.redmine_client = rm.RedMineClient("https://support.targetintegration.com",
                                                "c3a7f2f4562ed90ff5bc9b6d9e0574f4d434d54e")
+        self.label_status = ttk.Label(self.main_frame)
 
         self.create_ui()
         self.get_activities()
@@ -54,14 +53,19 @@ class Application(ttk.Frame):
         constpadx = 10
         constpady = 5
         constSticky = (Tkinter.W,)
-        ttk.Label(self.main_frame, textvariable=self.status_msg).grid(column=0, row=0, columnspan=3,
-                                                                      sticky=constSticky, padx=constpadx,
-                                                                      pady=constpady)
+        self.box = mbox.tbox(self)
+        # standard_font = label_status['font']
+        # new_font = tkFont.Font(font=standard_font)
+        # new_font['weight'] = tkFont.BOLD
+        # label_status['font'] = new_font
+        self.label_status.grid(column=0, row=0, columnspan=3,
+                               sticky=constSticky, padx=constpadx,
+                               pady=constpady)
         self.entry_issue.grid(column=0, row=1, columnspan=1, sticky=constSticky,
                               padx=constpadx, pady=constpady)
         self.btn_find_issue.grid(column=1, row=1, columnspan=1, sticky=constSticky, padx=constpadx, pady=constpady)
 
-        ttk.Button(self.main_frame, text="Settings").grid(column=2, row=1, columnspan=1, sticky=constSticky,
+        ttk.Button(self.main_frame, text="Settings", command=lambda: self.box("Name ?")).grid(column=2, row=1, columnspan=1, sticky=constSticky,
                                                           padx=constpadx,
                                                           pady=constpady)
         ttk.Label(self.main_frame, textvariable=self.issue_subject, wraplength=370).grid(column=0, row=2, columnspan=3,
@@ -90,6 +94,8 @@ class Application(ttk.Frame):
 
 
     def save_time_entry_click(self):
+        if not self.validate_form():
+            return
         selected_activity = self.cmb_activity.get()
         selected_activity_id = list(filter(lambda x: x["name"] == selected_activity, self.activities))[0]["id"]
         user_comments = self.box_comments.get('1.0', Tkinter.END)
@@ -97,18 +103,15 @@ class Application(ttk.Frame):
                                        comments=user_comments, time_in_minutes=self.time_in_minutes.get())
 
         self.after(2, self.process_time_entry)
-        # self.after()
-        # print(selected_activity_id)
-        # pass
 
     def process_time_entry(self):
         status = self.redmine_client.post_time_entry(self.time_entry)
         if status:
-            self.status_msg.set("Time Entry Saved")
+            self.set_success_msg("Time Entry Saved SuccessFully")
 
 
     def find_issue_click(self):
-        is_valid = self.validate_form()
+        is_valid = self.validate_issue_id()
         if not is_valid:
             return
         if self.issue is None:
@@ -120,19 +123,34 @@ class Application(ttk.Frame):
             self.issue_id.set("")
             self.issue_subject.set("")
 
-    def validate_form(self):
-        self.status_msg.set("")
+    def set_error_msg(self, message):
+        self.label_status['foreground'] = 'RED'
+        self.label_status['text'] = message
+
+    def set_msg(self, message):
+        self.label_status['foreground'] = 'BLACK'
+        self.label_status['text'] = message
+        pass
+
+    def set_success_msg(self, message):
+        self.label_status['foreground'] = 'GREEN'
+        self.label_status['text'] = message
+        pass
+
+    def validate_issue_id(self):
         is_issue_id_valid = self.issue_id.get().isdigit()
         if not is_issue_id_valid:
-            self.status_msg.set("Issue Id has to integer")
+            self.set_error_msg("Issue ID has to be integer")
         return is_issue_id_valid
-        # try:
-        #     int(self.issue_id.get())
-        # except ValueError:
-        #     self.status_msg.set("Issue Id has to integer")
-        # return True
 
-        # self.status_msg = "Empty Status Message"
+    def validate_form(self):
+        if self.issue is None:
+            self.set_error_msg("Please find an issue first")
+            return False
+        if not self.time_in_minutes.get().isdigit():
+            self.set_error_msg("Provide time in minutes")
+            return False
+        return True
 
     def req_find_issue(self):
         self.issue = self.redmine_client.get_issue(int(self.issue_id.get()))
@@ -143,7 +161,7 @@ class Application(ttk.Frame):
 
     def get_activities(self):
         self.activity_thread.__init__(target=self.req_get_activity_process, args=())
-        self.status_msg.set("Loading activities ... ")
+        self.set_msg("Loading Activities .. ")
         self.activity_thread.start()
         self.after(5, self.req_get_activity_end)
 
@@ -160,8 +178,7 @@ class Application(ttk.Frame):
             default_item = list(filter(lambda x: 'is_default' in x, self.activities))[0]["name"]
             self.cmb_activity['values'] = list(map(lambda x: x["name"].encode('ascii', 'ignore'), self.activities))
             self.cmb_activity.set(default_item)
-            # self.cmb_activity.bind(sequence=)
-            self.status_msg.set("")
+            self.set_msg("")
             self.activity_thread.join()
 
 
